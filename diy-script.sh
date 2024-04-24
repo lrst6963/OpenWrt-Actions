3b1cb4abbd26bfd3a6340d7e5d5b4fd6a069f207
CC <59593391+CC13594759@users.noreply.github.com>
2024-04-24 09:32:48 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -4,10 +4,10 @@
 sed -i 's/192.168.1.1/192.168.12.12/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+#sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+#sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
 rm -rf feeds/luci/themes/luci-theme-argon
