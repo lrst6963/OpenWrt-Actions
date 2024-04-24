0928f299984275f59d62d4718bed5334060d7d07
CC <59593391+CC13594759@users.noreply.github.com>
2024-04-24 22:42:54 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -43,9 +43,9 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 
 # 科学上网插件
 git clone --depth=1  https://github.com/fw876/helloworld package/luci-app-ssr-plus
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
