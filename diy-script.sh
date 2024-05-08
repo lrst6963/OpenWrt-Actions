4cdef987a23e67ddcb571a1c24afbec04b40004b
CC <59593391+CC13594759@users.noreply.github.com>
2024-05-08 20:50:13 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -46,9 +46,6 @@ sed -i "s/${orig_version}/R${date_version}/g" package/lean/default-settings/file
 sed -i 's/distversion)%>/distversion)%><!--/g' package/lean/autocore/files/*/index.htm
 sed -i 's/luciversion)%>)/luciversion)%>)-->/g' package/lean/autocore/files/*/index.htm
 
-# 修复 hostapd 报错
-cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
-
 # 修复 armv8 设备 xfsprogs 报错
 sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
 
