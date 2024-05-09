cc4a8f89484baa29b43273d618abd41d99ce6317
CC <59593391+CC13594759@users.noreply.github.com>
2024-05-10 00:09:02 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -19,7 +19,7 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/sbwml/luci-app-airconnect package/luci-app-airconnect
 
 # 科学上网插件
-git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
