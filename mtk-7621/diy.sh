bfad816a7995a0da2b0828b2cba96eb7ccef258d
CC <59593391+CC13594759@users.noreply.github.com>
2024-05-12 20:46:41 +0800

    Update diy.sh

--- mtk-7621/diy.sh
+++ mtk-7621/diy.sh
@@ -6,5 +6,3 @@ sed -i 's/192.168.1.1/192.168.12.1/g' openwrt/package/base-files/files/bin/confi
 # 添加额外插件
 git clone --depth=1 https://github.com/sbwml/luci-app-airconnect package/luci-app-airconnect
 
-./scripts/feeds update -a
-./scripts/feeds install -a
