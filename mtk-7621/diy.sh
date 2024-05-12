97cdb5da8652132331831505b093824eaf25facb
CC <59593391+CC13594759@users.noreply.github.com>
2024-05-12 22:15:47 +0800

    Update diy.sh

--- mtk-7621/diy.sh
+++ mtk-7621/diy.sh
@@ -3,6 +3,4 @@
 # 默认IP  192.168.12.1
 sed -i 's/192.168.1.1/192.168.12.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# 添加额外插件
-git clone --depth=1 https://github.com/sbwml/luci-app-airconnect package/luci-app-airconnect
 
