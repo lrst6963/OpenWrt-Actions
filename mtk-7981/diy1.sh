794fb4b9348449f77bcf8658f246d94d75682a3a
CC <59593391+CC13594759@users.noreply.github.com>
2024-05-14 23:55:23 +0800

    Update diy1.sh

--- mtk-7981/diy1.sh
+++ mtk-7981/diy1.sh
@@ -2,7 +2,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git airconnect https://github.com/sbwml/luci-app-airconnect.git" >> ./feeds.conf.default
+echo "src-git airconnect https://github.com/sbwml/luci-app-airconnect.git" >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
