58b075754af4681c9fdbb134d09d111b772aa47a
CC <59593391+CC13594759@users.noreply.github.com>
2024-05-18 11:12:06 +0800

    Update init-settings.sh

--- rockchip/init-settings.sh
+++ rockchip/init-settings.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# 将默认主题设置为luci-theme-argon
-# uci set luci.main.mediaurlbase='/luci-static/argon'
-# uci commit luci
-
 # 禁用IPv6 ULA 前缀
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
-
-# Check file system during boot
-# uci set fstab.@global[0].check_fs=1
-# uci commit fstab
 
 exit 0
